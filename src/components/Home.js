64208bddc1929cf91d9e3b87d143eecba1208874
MayPyone <maypyone015@gmail.com>
2024-02-03 23:31:46 +0630

    Update style for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,47 +7,7 @@ import navIcon3 from '../assets/img/nav-icon3.svg';
 import navIcon4 from '../assets/img/github-icon.svg';
 
 export const Home = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "a passionate software developer."];
-  const period = 1000;
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [text])
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
-
+ 
   return (
     <section className="banner" id="home">
       
@@ -57,10 +17,9 @@ export const Home = () => {
       </video> */}
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
-           
-                <h1><span className="greeting">Hi! I'm </span><span className="name">May Pyone</span>,<br></br> <span className="txt-rotate occ" dataPeriod="40" data-rotate='[ "full-stack developer" ]'><span className="wrap">{text}</span></span></h1>
+                <h1><span className="greeting">Hi! I'm </span><span className="name">May Pyone</span>,<br></br> <span className="txt-rotate occ" ><span className="wrap">a  passionate  software  developer.</span></span></h1>
                   <p>
-                  IΓÇÖm a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, donΓÇÖt hesitate to contact me.
+                  I'm a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, donΓÇÖt hesitate to contact me.
                    </p>
                 
                   <div className="social-icon">
